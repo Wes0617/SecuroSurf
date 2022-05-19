4f839842a6463ddc4cf0e2dc2badd67ca3cfb581
Wes <netmosfera@gmail.com>
2022-05-19 12:00:57 +0200

    cx freeze doesn't like dash in version name

--- securosurf/information.py
+++ securosurf/information.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 
 class CLASS:
     application_name: str = "SecuroSurf"
-    application_version: str = "2.0.0-dev"
-    application_full_name: str = "SecuroSurf 2.0.0-dev"
+    application_version: str = "2.0.0"
+    application_full_name: str = "SecuroSurf 2.0.0"
     application_mime: str = "application/securosurf-2"
 
 ########################################################################################################################
