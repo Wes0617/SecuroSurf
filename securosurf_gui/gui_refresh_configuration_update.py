47fd20a9044231414ee0b67583dafb55330549de
Wes <netmosfera@gmail.com>
2022-05-30 17:08:48 +0200

    Improve the configuration update frame refresh code.

--- securosurf_gui/gui_refresh_configuration_update.py
+++ securosurf_gui/gui_refresh_configuration_update.py
@@ -12,30 +12,50 @@ def FUNC(
     SC_last_update_attempt: float | None
 ):
     widget_update_frequency: tk.Text = window["update_frequency"]
-    widget_update_frequency.tk_update(text=f"Every {SC.update_frequency} seconds.")
+    widget_update_last_attempted: tk.Text = window["update_last_attempted"]
+    widget_update_last_successful: tk.Text = window["update_last_successful"]
 
     # ------------------------------------------------------------------------------------------------------------------
 
     if SC_last_update_attempt is None:
-        _text = "Never."
+        return
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    SC_last_update_attempt_age = time.time() - SC_last_update_attempt
+    SC_age = time.time() - SC.fetch_time
+    is_failure = SC_last_update_attempt != SC.fetch_time
+    is_paused = not is_failure and SC_age > SC.update_frequency + 10
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    if is_paused:
+        _text = f"Every {SC.update_frequency} seconds (Paused)."
     else:
-        last_attempt_age = round(time.time() - SC_last_update_attempt)
-        _text = "More than 5 minutes ago." if last_attempt_age > (5 * 60) else f"{last_attempt_age} seconds ago."
-    widget_update_last_attempted: tk.Text = window["update_last_attempted"]
+        _text = f"Every {SC.update_frequency} seconds."
+
+    widget_update_frequency.tk_update(text=_text)
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    if SC_last_update_attempt_age > (3 * 60):
+        _text = f"more than 3 minutes ago."
+    else:
+        _text = f"{round(SC_last_update_attempt_age)} seconds ago."
     widget_update_last_attempted.tk_update(text=_text)
 
     # ------------------------------------------------------------------------------------------------------------------
 
-    _failure_age = 0 if SC_last_update_attempt is None else SC_last_update_attempt - SC.fetch_time
-    if _failure_age > 5:
-        _appearance = tk.TextAppearance.ERROR
+    if SC_age > (3 * 60):
+        _text = f"More than 3 minutes ago."
     else:
-        SC_age = round(time.time() - SC.fetch_time)
-        update_idled = SC_age > SC.update_frequency
-        _appearance = tk.TextAppearance.WARNING if update_idled else tk.TextAppearance.SUCCESS
+        _text = f"{round(SC_age)} seconds ago."
 
-    _SC_age = round(time.time() - SC.fetch_time)
-    _text = "More than 5 minutes ago." if _SC_age > (5 * 60) else f"{_SC_age} seconds ago."
+    if is_failure:
+        _appearance = tk.TextAppearance.ERROR
+    elif is_paused:
+        _appearance = tk.TextAppearance.NORMAL
+    else:
+        _appearance = tk.TextAppearance.SUCCESS
 
-    widget_update_last_successful: tk.Text = window["update_last_successful"]
     widget_update_last_successful.tk_update(text=_text, appearance=_appearance)
